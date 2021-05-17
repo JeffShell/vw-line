7318746fdb85f64a0e64ee5b1037990e9ef66844
jeffshell <875899952@qq.com>
2021-05-17 10:34:53 +0800

    创建侧边栏功能实现

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,50 @@
 <template>
-<div>
-  hi jeff
-</div>
-</template>
+  <div>
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            unique-opened
+            router>
+          <el-menu-item index="/interblockchainshow">跨链</el-menu-item>
+        </el-menu>
+      </el-aside>
 
+      <el-container>
+        <el-main>
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+
+</template>
 <script>
 export default {
-  name: "Home.vue"
+
 }
 </script>
 
-<style scoped>
+<style leng="scss">
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
 </style>
