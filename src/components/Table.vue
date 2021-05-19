d766d81e01f94f9bc792d45459466453d2309bc2
jeffshell <875899952@qq.com>
2021-05-19 21:35:57 +0800

    代码妥协，放弃使用v-for

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,18 +1,26 @@
 <template>
   <div>
     <el-table :data="displayData" stripe>
-      <el-table-column
-          v-for="(item, index) in table_row" :key="index"
-          :prop="item.prop" :label="item.label">
-        <template v-if="item.prop === 'status'"  slot-scope="scope">
+      <el-table-column prop="licence" label="名称"></el-table-column>
+      <el-table-column prop="number" label="编号"></el-table-column>
+      <el-table-column prop="unit" label="建设单位"></el-table-column>
+      <el-table-column prop="department" label="发证机关"></el-table-column>
+      <el-table-column prop="date" label="发证日期"></el-table-column>
+      <el-table-column prop="unitName" label="工程名称"></el-table-column>
+      <el-table-column prop="address" label="建设地址"></el-table-column>
+      <el-table-column prop="price" label="合同价格"></el-table-column>
+      <el-table-column prop="buildUnit" label="建设单位"></el-table-column>
+      <el-table-column prop="hash" label="交易哈希"></el-table-column>
+      <el-table-column prop="status" label="状态">
+        <template slot-scope="scope">
           <el-tag :type="scope.row.status ? 'success' : 'info'">
-            {{scope.row.status ? '缓存' : '未缓存'}}
+            {{scope.row.status ? '已缓存' : '未缓存'}}
           </el-tag>
         </template>
-        <div class="cc" v-if="item.prop === 'cc'">
-          <el-button size="mini" type="success">上链</el-button>
-          <el-button size="mini" type="danger">删除</el-button>
-        </div>
+      </el-table-column>
+      <el-table-column prop="cc" label="操作">
+        <el-button size="mini" type="success">上链</el-button>
+        <el-button size="mini" type="danger">删除</el-button>
       </el-table-column>
     </el-table>
     <div class="tabListPage">
@@ -59,12 +67,18 @@ export default {
       PageSize: 5,
     }
   },
+  filters: {
+    capitalize: function (value) {
+      if (!value) return
+      console.log(value)
+    }
+  },
   computed: {
     displayData() {
       return this.lineData.slice(
           (this.currentPage - 1) * this.PageSize, this.currentPage * this.PageSize
       )
-    }
+    },
   },
   methods: {
     getData() {
@@ -80,8 +94,6 @@ export default {
       console.log(row)
       return row.tag === value;
     },
-
-
     handleSizeChange(val) {
       this.PageSize = val
       this.currentPage = 1
--- src/views/Nothing.vue
+++ src/views/Nothing.vue
@@ -1,12 +1,27 @@
 <template>
 <div>
   什么都没有，未来添加的页面。
+  {{message | capitalize}}
 </div>
 </template>
 
 <script>
 export default {
-  name: "Noting.vue"
+  name: "Noting.vue",
+  data(){
+    return {
+      message : 'tom'
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+
+
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  }
 }
 </script>
 
