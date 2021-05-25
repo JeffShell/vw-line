b03e16ed82b782afecbba665fa806926845a4bea
jeffshell <875899952@qq.com>
2021-05-25 10:16:20 +0800

    TableInquire组件化

--- src/components/BlockchainForm.vue
+++ src/components/BlockchainForm.vue
@@ -34,40 +34,6 @@
         </el-col>
       </el-form>
     </el-row>
-    <el-row :gutter="20">
-      <el-form class="riqi" ref="form" :model="form" label-width="100px">
-        <el-col :span="4">
-          <el-input placeholder="工程名单"></el-input>
-        </el-col>
-        <el-col :span="4">
-          <el-input placeholder="施工单位"></el-input>
-        </el-col>
-        <el-col :span="4">
-          <el-input placeholder="发证机关"></el-input>
-        </el-col>
-        <el-col :span="8">
-          <div class="block">
-            <span class="demonstration">查询</span>
-            <el-date-picker
-                type="date"
-                placeholder="选择日期">
-            </el-date-picker>
-            <span class="demonstration">至</span>
-            <el-date-picker
-                type="date"
-                placeholder="选择日期">
-            </el-date-picker>
-          </div>
-        </el-col>
-        <el-col :span="2">
-
-          <el-button type="primary" @click="onSubmit">
-            <i class="el-icon-search"></i>
-            搜索
-          </el-button>
-        </el-col>
-      </el-form>
-    </el-row>
   </div>
 </template>
 
--- src/views/InterBlockchainShow.vue
+++ src/views/InterBlockchainShow.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <BlockchainForm/>
+    <TableInquire/>
     <Table/>
   </div>
 </template>
 
 <script>
 import BlockchainForm from "@/components/BlockchainForm.vue"
+import TableInquire from "@/components/TableInquire.vue"
 import Table from "@/components/Table.vue"
 export default {
   components: {
     BlockchainForm,
+    TableInquire,
     Table
   }
 }
