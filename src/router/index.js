adc376678f1884840995ac33c94d18c4cd22a5aa
jeffshell <875899952@qq.com>
2021-06-01 10:23:36 +0800

    完成登录页面样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,45 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/Login';
 import Home from "@/views/Home.vue";
 import InterBlockchainShow from "@/views/InterBlockchainShow.vue";
-import Nothing from "@/views/Nothing.vue"
+import Nothing from "@/views/Nothing.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        redirect: "/interblockchainshow",
-        name: "首页"
-    },
-    {
-        path: '/home',
-        redirect: "/home/interblockchainshow",
-        name: "首页"
-    },
-    {
-        path: "/home",
-        component: Home,
-        name: "首页",
-        children: [{
-            path: '',
-            component: InterBlockchainShow,
-        },{
-            path: '/interblockchainshow',
-            component: InterBlockchainShow,
-            name: '跨链',
-        },{
-            path: '/nothing',
-            component: Nothing,
-            name: '什么都没',
-        }]
-    }
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: "/interblockchainshow",
+    name: "首页"
+  },
+  {
+    path: '/home',
+    redirect: "/home/interblockchainshow",
+    name: "首页"
+  },
+  {
+    path: "/home",
+    component: Home,
+    name: "首页",
+    children: [{
+        path: '',
+        component: InterBlockchainShow,
+    },{
+        path: '/interblockchainshow',
+        component: InterBlockchainShow,
+        name: '跨链',
+    },{
+        path: '/nothing',
+        component: Nothing,
+        name: '什么都没',
+    }]
+  }
 
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,68 @@
+<template>
+<el-row class="main" type="flex" justify="center" align="middle">
+  <el-col :span="8" :md="7">
+    <el-card>  
+      <el-form 
+        ref="formdata"
+        :model="formdata" 
+        label-width="80px" 
+        :rules="rules">
+        <el-form-item label="用户名" prop="user">
+          <el-input v-model="formdata.user" placeholder="请输入用户名"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="passwd">
+          <el-input v-model="formdata.passwd" placeholder="请输入密码" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="validate('formdata')">
+            登录
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </el-col>
+</el-row>
+
+</template>
+<script>
+export default {
+  data(){
+    return {
+      formdata: {
+        user: '',
+        passwd: '',
+      },
+      rules: {
+        passwd: [
+          { required: true, message: '请输入', trigger: 'blur' },
+          { max: 5, message: "请输入至少5个数字" }
+        ]
+      }
+    }
+  },
+  methods:{
+    validate(value){
+      this.$refs[value].validate((valid) => {
+        if (valid) {
+          console.log(valid);
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+
+}
+</script>
+<style lang="scss">
+body {
+  background: #44a08d;
+  background: -webkit-linear-gradient(to top, #44a08d, #093637); 
+  background: linear-gradient(to top, #44a08d, #093637);
+}
+.main {
+  height: 100vh;
+  margin-top: 0px;
+}
+</style>
